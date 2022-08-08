a9ce5f3663ee93769930289f7ff99dff088815d1
Andrigo Damian Lopes <andrigodamianlopes@hotmail.com>
2022-08-08 13:54:26 -0300

    Adicionando novos exemplos.

---
+++ Meus Exercicios/Ex001/index.js
@@ -0,0 +1,8 @@
+function media(){
+    let a = Number(prompt("Valor de A: "))
+    let b = Number(prompt("Valor de B:"))
+    mediaNotas = (a + b) / 2
+    alert("Media dos valores " + a + " e " + b + " = " + mediaNotas)
+}
+ media()
+
---
+++ Meus Exercicios/Ex002/index.js
@@ -0,0 +1,4 @@
+let numeroTab = prompt("Qual tabuada: ")
+for(i=1; i<11; i++){
+    console.log(numeroTab + " x " + i + " = " + (numeroTab*i))
+}
